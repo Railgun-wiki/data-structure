4dd6e64af46d12a92f1e58555ed431637d546bc3
YukiShan <laoshc@mail2.sysu.edu.cn>
2026-03-17 16:52:01 +0800

    Solve P3

---
+++ 3.无重复字符的最长子串.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=3 lang=python3
+#
+# [3] 无重复字符的最长子串
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = 0
+        left = 0
+        char_set = set()
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+            char_set.add(s[right])
+            ans = max(ans, right - left + 1)
+        return ans
+# @lc code=end
+
