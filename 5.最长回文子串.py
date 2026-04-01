1a82ebe876662ce267f68a46c14ba2acba25b161
YukiShan <laoshc@mail2.sysu.edu.cn>
2026-04-01 20:54:06 +0800

    ignore .vscode\ and  add settings.json

--- 5.最长回文子串.py
+++ 5.最长回文子串.py
@@ -8,21 +8,8 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         ansright, ansleft = 0, 0
-        for i in range(len(s)):
-            left, right = i, i
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            if right - left - 2 > ansright - ansleft:
-                ansright, ansleft = right - 1, left + 1
+        for 
 
-            left, right = i, i + 1
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            if right - left - 2 > ansright - ansleft:
-                ansright, ansleft = right - 1, left + 1
-        return s[ansleft:ansright + 1]
 
 # @lc code=end
 
